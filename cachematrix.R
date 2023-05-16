c47b88b890d17e4a196622286091c5d64e2c42e9
nphuong302 <pnnp2031997@gmail.com>
2023-05-17 00:08:01 +0700

    update check singular matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+##  The function generates a cache for a matrix that may be used to store the matrix and its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -13,9 +15,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve() function computes the inverse of a matrix and caches the result in a cache built by the makeCacheMatrix() function. 
 
-cacheinverse <- function(x, ...) {
+cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data")
